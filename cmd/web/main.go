93b3fe15953c60f3e0972fe8f10545413ac1267e
Viam Ahmadi <viamahmadi@gmail.com>
2021-01-23 17:22:45 +0330

    Templating and Create handlers

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/post", showPost)
+	mux.HandleFunc("/post/create", createPost)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
