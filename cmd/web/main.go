4acaec6931ddc1927fcd31f55f61ead71e924b01
Viam Ahmadi <viamahmadi@gmail.com>
2021-01-24 20:50:02 +0330

    Designing Database model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"github.com/viamAhmadi/mars/pkg/models/mysql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	posts    *mysql.PostModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "marsuser:mars123@/mars?parseTime=true", "Mysql database")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		posts:    &mysql.PostModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -31,6 +44,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on :%s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
