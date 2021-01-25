72b9879577ebbddb1c4dbb14b54275db35f19dfe
Viam Ahmadi <viamahmadi@gmail.com>
2021-01-25 17:41:18 +0330

    Writing posts model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"github.com/viamAhmadi/mars/pkg/models"
 	"net/http"
 	"strconv"
 )
@@ -13,23 +13,33 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
+	p, err := app.posts.Latest()
 	if err != nil {
-		app.errorLog.Println(err.Error())
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err)
+	for _, post := range p {
+		fmt.Fprintf(w, "%v\n", post)
 	}
+
+	//files := []string{
+	//	"./ui/html/home.page.tmpl",
+	//	"./ui/html/base.layout.tmpl",
+	//	"./ui/html/footer.partial.tmpl",
+	//}
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.errorLog.Println(err.Error())
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//err = ts.Execute(w, nil)
+	//if err != nil {
+	//	app.errorLog.Println(err.Error())
+	//	app.serverError(w, err)
+	//}
 }
 
 func (app *application) showPost(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +49,16 @@ func (app *application) showPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific post with ID %d", id)
+	p, err := app.posts.Get(id)
+	if err == models.ErrNoRecord {
+		app.notFound(w)
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", p)
 }
 
 func (app *application) createPost(w http.ResponseWriter, r *http.Request) {
@@ -49,5 +68,15 @@ func (app *application) createPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new post"))
+	title := "the title"
+	content := "content of this post"
+	expires := "10"
+
+	id, err := app.posts.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/post?id=%d", id), http.StatusSeeOther)
 }
--- pkg/models/mysql/posts.go
+++ pkg/models/mysql/posts.go
@@ -10,13 +10,53 @@ type PostModel struct {
 }
 
 func (m *PostModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO posts (title, content, created, expires) VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *PostModel) Get(id int) (*models.Post, error) {
-	return nil, nil
+	p := &models.Post{}
+	row := m.DB.QueryRow(`SELECT id, title, content, created, expires FROM posts WHERE expires > UTC_TIMESTAMP() AND id=?`, id)
+	err := row.Scan(&p.ID, &p.Title, &p.Content, &p.Created, &p.Expires)
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
 
 func (m *PostModel) Latest() ([]*models.Post, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM posts WHERE expires>UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var posts []*models.Post
+
+	for rows.Next() {
+		p := &models.Post{}
+		err = rows.Scan(&p.ID, &p.Title, &p.Content, &p.Created, &p.Expires)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return posts, nil
 }
